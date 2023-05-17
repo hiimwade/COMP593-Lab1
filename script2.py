ee19a747b568ca5ea3a895a20a2dbb4e0482f60e
Jeff-Newmarch <wade_manley2@outlook.com>
2023-05-17 15:37:56 -0400

    Modified both files in the repo clone
    Deleted a print() statement from script1.py
    Added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
 # Print lines from Zen of Python
 print("There should be one-- and perferably only one --obvious way to do it.")
+print("Although that way may not be obvious at first unless you're Dutch")
+print("Namespaces are one honking great idea -- let's do more of those!")
